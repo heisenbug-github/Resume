78d917509efa37a19a622ea02db7ae574580f032
Hasan Kaplan <Hasan Kaplan>
2019-09-17 20:10:28 +0300

    ConnectionString tekrar açıldı. Index migrate edildi.

---
+++ Resume.DbContext/Migrations/20190917170830_degisiklikler.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Resume.DbContext.Migrations
+{
+    public partial class degisiklikler : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_message_clustered_index",
+                table: "message",
+                column: "clustered_index",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_message_clustered_index",
+                table: "message");
+        }
+    }
+}
